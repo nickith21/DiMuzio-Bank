343faeb35bb337c4ccb114315c532b486aa4f76c
Nick <ndd243@gmail.com>
2023-02-01 19:24:10 -0500

    experiments with footer

--- public/cookieWarning.js
+++ public/cookieWarning.js
@@ -6,10 +6,7 @@ const CookieWarning = () => {
   return (
     <>
       {show && (
-        <div
-          
-          className="ot-sdk-row fixed-bottom navbar-light bg-light"
-        >
+        <div className="ot-sdk-row fixed-bottom navbar-light bg-light">
           <div
             id="onetrust-group-container"
             className="ot-sdk-eight ot-sdk-columns"
@@ -47,49 +44,6 @@ const CookieWarning = () => {
               </button>
             </div>
           </div>
-        </div>
-      )}
-      {!show && (
-        <div className="container fixed-bottom bg-light">
-          <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div className="col-md-4 d-flex align-items-center">
-              <a
-                href="/"
-                className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-              >
-                <svg className="bi" width="30" height="24">
-                  {/* <use xlink:href="#bootstrap" /> */}
-                </svg>
-              </a>
-              <span className="mb-3 mb-md-0 text-muted">
-                &copy; 2023 DiMuzio Bank, Inc
-              </span>
-            </div>
-
-            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#twitter" /> */}
-                  </svg>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#instagram" /> */}
-                  </svg>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="text-muted" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#facebook" /> */}
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </footer>
         </div>
       )}
     </>
---
+++ public/light-dark.js
@@ -0,0 +1,14 @@
+function LightDarkToggle() {
+  return (
+    <>
+      <div class="custom-control custom-switch">
+        <input
+          type="checkbox"
+          class="custom-control-input"
+          id="customSwitch1"
+        />
+        <label class="custom-control-label" for="customSwitch1">Light/Dark</label>
+      </div>
+    </>
+  );
+}
